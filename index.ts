cddda41252eb3c5610c024814009771af9d9d2ee
Mradul Raj Singh <mradulrajsingh61@gmail.com>
2026-04-21 14:34:03 +0530

    debugging the deployment

--- index.ts
+++ index.ts
@@ -4,6 +4,10 @@ import app from './app.js';
 import { initSocket } from './socket/index.js';
 
 const PORT = process.env.PORT ?? 8080;
+console.log('ENV CHECK:', {
+  url: process.env.SUPABASE_URL,
+  key: process.env.SUPABASE_PUBLISHABLE_KEY,
+});
 
 const server = createServer(app);
 
